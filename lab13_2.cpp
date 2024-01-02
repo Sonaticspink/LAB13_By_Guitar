ebb9ec090c103f07e35fae903f46f02441842a25
Guitar <keetakeeta59@gmail.com>
2024-01-02 16:31:06 +0700

    LAB13_2 COMPLETED

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,33 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][M],int s,int x,int y){
+    for(int n = 0 ; n < N ; n++){
+        for(int m = 0 ; m < M ; m++){
+            if(sqrt(pow((n-x),2)+(pow((m-y),2))) <= s-1)
+            image[n][m] = 1 ;
+        }
+}
+}
+
+void showImage(const bool image[][M]){
+    for(int i=0;i<M+2;i++) cout<<"-";
+    cout<<endl;
+    for(int y = 0 ; y < 30 ; y++){
+        cout<<"|";
+        for(int x = 0 ; x < 70 ; x++){
+            if(image[y][x] == 0){
+                cout<<" ";
+            }
+            else if(image[y][x] == 1){
+                cout<<"*";
+            }
+        }
+        cout<<"|"<<endl;
+    }
+    for(int i=0;i<M+2;i++) cout<<"-";
+    cout<<endl;
+} 
+
+
