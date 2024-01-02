b6cb01e0172f127313f87cc2b6c8f16a09c7f88a
Guitar <keetakeeta59@gmail.com>
2024-01-02 15:22:16 +0700

    LAB13_1 COMPLETED

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <limits.h>
 using namespace std;
 
 void stat(const double[],int,double[]);
@@ -16,4 +17,22 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double X[],int Y,double Z[]){
+    Z[4] = INT_MIN; Z[5] = INT_MAX;
+    Z[1] = 0; Z[2] = 1;
+    for(int i = 0 ; i < Y ; i++){ 
+    Z[0] += X[i];
+    Z[2] *= X[i];
+    Z[3] += (1/X[i]);
+    if(X[i] > Z[4]) Z[4] = X[i];
+    if(X[i] < Z[5]) Z[5] = X[i];
+    }
+    Z[0] /= Y;
+    for(int i=0 ; i<Y ; i++) Z[1] += pow(X[i]-Z[0],2);
+    Z[1] = sqrt((Z[1])/(Y));
+    Z[2] = pow(Z[2],1.0/Y);
+    Z[3] = Y/Z[3];
+
 }
